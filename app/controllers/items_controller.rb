52267db835a774395628c7e8476517673177b268
Jerry Buck <jerry.buck@pt.lu>
2023-06-06 17:31:31 +0200

    Ajax working in searchbar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,17 @@ class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_item, only: [:show]
   def index
-    @items = Item.all
+    if params[:query].present?
+      sql_query = "name @@ :query OR description @@ :query OR category @@ :query OR room_type @@ :query"
+      @items = Item.where(sql_query, query: "%#{params[:query]}%").limit(30)
+    else
+      @items = Item.all
+    end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'items/list', locals: { items: @items }, formats: [:html] }
+    end
   end
 
   def show
