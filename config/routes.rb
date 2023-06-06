c6665b8c0aec05e7b317ceadec181e5c44b966e7
Manon Roussel <manonmsroussel@gmail.com>
2023-06-06 12:56:12 +0200

    doodle

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  if Rails.env.development?
+    get '/components', to: "pages#components"
+  end
+
   resources :items
 end
