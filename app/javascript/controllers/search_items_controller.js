1d6df186b243ef4f395d5ca95cd127458ac79a39
Jerry Buck <jerry.buck@pt.lu>
2023-06-07 11:08:03 +0200

    Added functionalities to searchbar

--- app/javascript/controllers/search_items_controller.js
+++ app/javascript/controllers/search_items_controller.js
@@ -5,10 +5,6 @@ export default class extends Controller {
   static targets = [ "form", "list", "searchInput" ]
 
   connect() {
-    console.log(this.element);
-    console.log(this.formTarget);
-    console.log(this.listTarget);
-    console.log(this.searchInputTarget);
   }
 
   update() {
