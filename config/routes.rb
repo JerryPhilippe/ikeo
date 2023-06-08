97a8cf9e37e18cec8427fcab5d2b759639cda010
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-06-08 15:27:18 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   resources :items
   get "/cart", to: "cart_items#index"
   get "/item/:id/create", to: "cart_items#create", as: :create_cart_item
+
+  resources :user, only: [] do
+    resources :games, only: [:update]
+  end
 end
