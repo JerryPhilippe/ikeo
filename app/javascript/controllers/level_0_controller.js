76c5afeba91d178006fb81e63ad26216e2a19fd3
Jerry Buck <jerry.buck@pt.lu>
2023-06-08 20:46:35 +0200

    Added flashlight -- not functional

--- app/javascript/controllers/level_0_controller.js
+++ app/javascript/controllers/level_0_controller.js
@@ -5,7 +5,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="level-0"
 export default class extends Controller {
-  static targets = ["bubble", "progression"]
+  static targets = ["bubble", "progression", "mouse"]
 
   connect() {
     if (window.location.pathname == "/"){
@@ -23,6 +23,14 @@ export default class extends Controller {
 
   okButton(){
     this.bubbleTarget.classList.toggle("d-none");
+  }
+
+  move(e) {
+    let mouseY = e.clientY
+    let mouseX = e.clientX
+    this.mouseTarget.style.background = "none";
+    this.mouseTarget.style.backgroundImage = "radial-gradient(circle closest-corner at " + mouseX + "px " + mouseY + "px , rgba(0,0,0,0) 0%, rgba(0,0,0,.9) 66%)";
+    console.log(mouseX, mouseY)
   }
 
   updateProgressBar(desiredWidth, progressTarget) {
