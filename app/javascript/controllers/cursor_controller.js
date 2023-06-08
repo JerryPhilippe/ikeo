23b3ba29925489c7933fe153626a4e8781de099c
evans hulot <hulotevans@gmail.com>
2023-06-08 18:06:13 +0200

    add flashlight first enigm

--- app/javascript/controllers/cursor_controller.js
+++ app/javascript/controllers/cursor_controller.js
@@ -2,12 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="cursor"
 export default class extends Controller {
-  static targets = ["mouse", "background"]
+  static targets = ["mouse"]
 
   move(e) {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
-    this.mouseTarget.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-    console.log(this.mouseTarget)
+    let mouseY = e.clientY
+    let mouseX = e.clientX
+    this.mouseTarget.style.background = "none";
+    this.mouseTarget.style.backgroundImage = "radial-gradient(circle closest-corner at " + mouseX + "px " + mouseY + "px , rgba(0,0,0,0) 0%, rgba(0,0,0,.9) 66%)";
+    console.log(mouseX, mouseY)
   }
 }
