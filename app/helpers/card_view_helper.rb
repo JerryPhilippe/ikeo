93c7305c672fe941744c55773fa00e2b0911a5be
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-06-12 12:38:41 +0200

    -card_item_helper

--- app/helpers/card_view_helper.rb
+++ app/helpers/card_view_helper.rb
@@ -3,8 +3,6 @@ module CardViewHelper
     case game.level
     when 0
       return "click->level-0#switchLight" if item.name.downcase == "lampküs"
-    when 1
-      return "click->level-1#returncard" if item.name.downcase == ""
     end
   end
 end
