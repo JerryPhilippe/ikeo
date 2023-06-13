be5e17a8f4e80ade67c30eea1b7e81b2ff304fa0
Jerry Buck <jerry.buck@pt.lu>
2023-06-13 09:25:28 +0200

    Part 2 level 2 + bubble

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import Typed from "typed.js";
 
 const level0Home = ["psst psst", "hey!", "psst","Hej ! J’ai besoin d’aide ! Que quelqu’un allume la lumière ! J’ai peur du noir !!! Je sais qu’il y a une lampe dans les articles de salon, qui que tu sois peux-tu aller l’allumer ?"]
-
+const level2Cart = ["Bien joué !", "Une clef se trouve caché sur le site", "Celle-ci te permettra d'accéder à une pièce secrete. Trouve la vite afin que je puisse quitter le site"]
 // Connects to data-controller="typed"
 export default class extends Controller {
   static values = {
@@ -12,12 +12,17 @@ export default class extends Controller {
   connect() {
     switch(this.levelValue){
       case 0 :
-        const options = {strings: level0Home, typeSpeed: 50};
+        const options0 = {strings: level0Home, typeSpeed: 50};
         setTimeout(() => {
-          new Typed(this.element, options);
+          new Typed(this.element, options0);
       }, 5500);
-        break;
+      break;
+      case 2 :
+        const options2 = {strings: level2Cart, typeSpeed: 50};
+        setTimeout(() => {
+          new Typed(this.element, options2);
+      }, 5500);
+      break;
     }
-
   }
 }
