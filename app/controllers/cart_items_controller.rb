3d5814660679916edce22fa2017fa9b22ee81657
Jerry Buck <jerry.buck@pt.lu>
2023-06-15 10:52:46 +0200

    Added devise.fr.yml and configured locals to french

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,8 +6,8 @@ class CartItemsController < ApplicationController
   def create
     @item = Item.find(params[:id])
     if CartItem.where(item_id: @item.id, cart_id: current_user.cart.id).exists?
-        flash[:alert] = "Koko possède déjà cet article"
-        redirect_to item_path(@item)
+      flash[:alert] = "Koko possède déjà cet article"
+      redirect_to item_path(@item)
     else
       if @item.name.downcase == "lampküs" || @item.name.downcase == "sac à dos"
         CartItem.create(cart: current_user.cart, item: @item)
